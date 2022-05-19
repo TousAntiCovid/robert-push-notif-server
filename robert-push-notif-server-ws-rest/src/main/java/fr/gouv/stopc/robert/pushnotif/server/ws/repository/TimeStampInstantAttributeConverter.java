6bc97fce193b3788a752599f4f2027e8f82169e6
Bergamote Orange <13178-x-BOrang@users.noreply.gitlab.inria.fr>
2022-05-19 11:55:49 +0200

    refacto: LocalDateTime to Instant

---
+++ robert-push-notif-server-ws-rest/src/main/java/fr/gouv/stopc/robert/pushnotif/server/ws/repository/TimeStampInstantAttributeConverter.java
@@ -0,0 +1,19 @@
+package fr.gouv.stopc.robert.pushnotif.server.ws.repository;
+
+import javax.persistence.AttributeConverter;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class TimeStampInstantAttributeConverter implements AttributeConverter<Instant, Timestamp> {
+
+    @Override
+    public Timestamp convertToDatabaseColumn(Instant attribute) {
+        return attribute == null ? null : Timestamp.from(attribute);
+    }
+
+    @Override
+    public Instant convertToEntityAttribute(Timestamp dbData) {
+        return dbData == null ? null : dbData.toInstant();
+    }
+}
