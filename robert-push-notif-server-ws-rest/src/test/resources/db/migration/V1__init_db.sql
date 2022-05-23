4a1cba6ac3ed5c543a55f6189ea013d7305ea2b5
Bergamote Orange <13178-x-BOrang@users.noreply.gitlab.inria.fr>
2022-05-23 15:25:42 +0200

    refacto: use prod like db schema in tests

--- robert-push-notif-server-ws-rest/src/test/resources/db/migration/V1__init_db.sql
+++ robert-push-notif-server-ws-rest/src/test/resources/db/migration/V1__init_db.sql
@@ -1,19 +1,23 @@
+create sequence push_id_seq;
+
 create table push
 (
-    id                   bigserial    not null,
+    id                   bigint not null default nextval('push_id_seq'::regclass),
     active               boolean,
-    creation_date        timestamp,
+    creation_date        timestamp without time zone,
     deleted              boolean,
-    failed_push_sent     int4,
-    last_error_code      varchar(255),
-    last_failure_push    timestamp,
-    last_successful_push timestamp,
-    locale               varchar(255) not null,
-    next_planned_push    timestamp,
-    successful_push_sent int4,
-    timezone             varchar(255) not null,
-    token                varchar(255) not null,
+    failed_push_sent     integer,
+    last_error_code      character varying(255),
+    last_failure_push    timestamp without time zone,
+    last_successful_push timestamp without time zone,
+    locale               character varying(255) not null,
+    next_planned_push    timestamp without time zone,
+    successful_push_sent integer,
+    timezone             character varying(255) not null,
+    token                character varying(255) not null unique,
     primary key (id)
 );
+
+alter sequence push_id_seq owned by push.id;
 
 create index IDX_TOKEN on push (token);
